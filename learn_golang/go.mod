5b68cdb16426a48d9e1a1d0c2212b79624d84262
abir <iamabir444@gmail.com>
2026-03-10 06:53:21 +0600

    struct & interface

---
+++ learn_golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/fahimabir8/lear_golang
+
+go 1.25.0
---
+++ learn_golang/struct.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type car struct {
+	brand string
+	model string
+}
+
+type truck struct {
+	car 
+	pupil int
+}
+
+func main() {
+	myTruck := truck {
+		pupil: 20,
+		car: car {
+			brand: "Axis",
+			model: "301-B",
+		},
+		
+	}
+	fmt.Println(myTruck)
+	
+	circ := circle {
+		radius: 5,
+	}
+	printShapeData(circ)
+}
